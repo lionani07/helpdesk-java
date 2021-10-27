969052592abb0d708ec55612cf6b7b45ba961577
lsanchez <lionani07@gmail.com>
2021-10-27 19:28:37 -0300

    first commit

---
+++ src/test/java/com/lionani07/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lionani07.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
